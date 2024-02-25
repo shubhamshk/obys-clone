880ab8810881490550af001a5ee1f1ac278cc387
Shubham Kumar <Shubhamshk07@gmail.com>
2024-02-25 22:28:00 +0530

    done till loader

---
+++ script.js
@@ -0,0 +1,67 @@
+function loadingAnimation(){
+    var tl = gsap.timeline()
+tl.from(".line h1",{
+    y:150,
+    stagger:0.25,
+    duration: 0.6,
+    delay: 0.3,
+});
+tl.from("#line1-part1",{
+    opacity:0,
+    onStart:function(){
+        var h5timer = document.querySelector("#line1-part1 h5")
+var grow = 0;
+setInterval(function(){
+    if(grow<100){
+        grow++
+        h5timer.innerHTML = grow++;
+
+    }else{
+        grow=100
+        h5timer.innerHTML = grow;
+    }
+    
+},35);
+
+
+    },
+});
+
+tl.to('.line h2',{
+    animationName: "anime",
+    opacity:1
+})
+
+tl.to("#loader",{
+    opacity:0,
+    duration:0.4,
+    delay: 0
+});
+
+tl.from("#page1",{
+    delay:0.2,
+    y:1600,
+    opacity:0,
+    duration:0.4,
+    ease: Power4
+})
+tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1",{
+    y: 120,
+    stagger:0.2
+
+})
+}
+loadingAnimation();
+
+function CursorAnimation(){
+    document.addEventListener("mousemove", function(dets){
+        gsap.to("#crsr",{
+            left:dets.x,
+            top:dets.y
+        })
+    
+    });
+    Shery.makeMagnet("#nav-part2 h4");
+}
+
+CursorAnimation();
